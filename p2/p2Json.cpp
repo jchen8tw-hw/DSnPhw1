6c470e7f286738017a3ff227ba5e24344612d205
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2018-08-27 15:54:38 +0800

    first commit

---
+++ p2/p2Json.cpp
@@ -0,0 +1,27 @@
+/****************************************************************************
+  FileName     [ p2Json.cpp ]
+  PackageName  [ p2 ]
+  Synopsis     [ Define member functions of class Json and JsonElem ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2018-present DVLab, GIEE, NTU, Taiwan ]
+****************************************************************************/
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "p2Json.h"
+
+using namespace std;
+
+// Implement member functions of class Row and Table here
+bool Json::read(const string& jsonFile){
+  ifstream infile(jsonFile);
+  if(infile.is_open()== false ) return false;
+  return true; // TODO
+}
+
+ostream&
+operator << (ostream& os, const JsonElem& j)
+{
+   return (os << "\"" << j._key << "\" : " << j._value);
+}
+
